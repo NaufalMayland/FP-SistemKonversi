bac69a54568ea1d74c870c7e3bf8b53e574d7497
NaufalMayland <maylandzaa13@gmail.com>
2025-12-23 20:31:13 +0700

    kondisi perhitungan konversi

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 void konversiMataUang(){
-    system("cls");
+    string menuMataUang[6] = {
+        "Rupiah(IDR)", 
+        "Ringgit(MYR)", 
+        "Dolar(USD)", 
+        "Yen(JPY)", 
+        "Won(KRW)", 
+        "Kembali ke menu utama"
+    };
 
-    string menuMataUang[6] = {"Rupiah(IDR)", "Ringgit(MYR)", "Dolar(USD)", "Yen(JPY)", "Won(KRW)", "Kembali ke menu utama"};
-    int pilih, pilihKonversi;
+    int pilih, pilihKonversi, nilai;
+    // float hasil;
 
     cout << "======================================" << endl;
     cout << "KONVERSI MATA UANG" << endl;
@@ -18,29 +25,83 @@ void konversiMataUang(){
     cout << "Pilih: ";
     cin >> pilih;
 
+    int pilihIndex = pilih - 1;
+    int tujuanKonversi[4];
+    int nomor = 0;
+
     if (pilih == 1){
-        cout << "\n" << menuMataUang[0] << " dikoversi ke: " << endl;
-        for (int i = 1; i <= 4; i++){
-            cout << i << ". " << menuMataUang[i] << endl;
+        cout << "\n" << menuMataUang[pilih - 1] << " dikoversi ke: " << endl;
+        for (int i = 0; i <= 4; i++){
+            if (i == pilihIndex){
+                continue;
+            }
+
+            cout << nomor + 1 << ". " << menuMataUang[i] << endl;
+            tujuanKonversi[nomor] = i;
+            nomor++;
         }
         cout << "Pilih: ";
         cin >> pilihKonversi;
-    }
+
+        int indexTujuan = tujuanKonversi[pilihKonversi - 1];
+        cout << "\nMasukkan jumlah nilai " << menuMataUang[pilih - 1] << " untuk dikonversi ke " << menuMataUang[indexTujuan];
+        cin >> nilai;
+
+    } 
 }
 
 void konversiSuhu(){
-    system("cls");
+    string menuSuhu[5] = {
+        "Celcius", 
+        "Fahrenheit", 
+        "Kelvin", 
+        "Kembali ke menu utama"
+    };
 
-    string menuSuhu[5] = {"Celcius", "Fahrenheit", "Kelvin", "Reamur", "Kembali ke menu utama"};
+    int pilih, pilihKonversi;
+    double nilai, hasil;
 
     cout << "======================================" << endl;
     cout << "KONVERSI SUHU" << endl;
     cout << "======================================" << endl;
     cout << "Pilih suhu yang ingin dikonversi:" << endl;
-    for (int i = 0; i <= 4; i++){
+    for (int i = 0; i <= 3; i++){
         cout << i + 1 << ". " << menuSuhu[i] << endl;
     }
     cout << "======================================" << endl;
+    cout << "Pilih: ";
+    cin >> pilih;
+
+    int pilihIndex = pilih - 1;
+    int tujuanKonversi[3];
+    int nomor = 0;
+
+    if (pilih == 1){
+        cout << "\n" << menuSuhu[pilih - 1] << " dikoversi ke: " << endl;
+        for (int i = 0; i <= 2; i++){
+            if (i == pilihIndex){
+                continue;
+            }
+        
+            cout << nomor + 1 << ". " << menuSuhu[i] << endl;
+            tujuanKonversi[nomor] = i;
+            nomor++;
+        }
+        cout << "Pilih: ";
+        cin >> pilihKonversi;
+
+        int indexTujuan = tujuanKonversi[pilihKonversi - 1];
+        cout << "\nMasukkan jumlah nilai " << menuSuhu[pilih - 1] << " untuk dikonversi ke " << menuSuhu[indexTujuan] << ": ";
+        cin >> nilai;
+
+        if (pilihKonversi == 1){
+            hasil = (nilai * 9 / 5) + 32;
+        } else if (pilihKonversi == 2){
+            hasil = nilai + 273.15;
+        }
+
+        cout << nilai << " " << menuSuhu[pilih - 1] << " = " << hasil << " " << menuSuhu[indexTujuan] << endl;
+    }
 }
 
 void mainMenu(){
@@ -69,9 +130,11 @@ void mainMenu(){
     system("cls");
 
     if (pilihMenu == 1) {
+        system("cls");
         konversiMataUang();
     } 
     else if (pilihMenu == 2) {
+        system("cls");
         konversiSuhu();
     } 
     else {
